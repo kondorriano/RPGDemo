1402813ac82c5290814b89a36ecb55f73fb17e9f
Nicolas Delgado <nicobito@gmail.com>
2020-02-26 13:57:06 +0100

    Initial commit

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private IEnumerator GameLoop()
+    {
+        yield return null;
+    }
+
+
+}
